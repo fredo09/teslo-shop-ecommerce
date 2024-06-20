51b28c4ef501fb9e35be3697a959ab5e7f7846f4
fredo09 <fred_15@outlook.com>
2024-06-19 21:35:36 -0600

    add view product slug in product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -2,10 +2,64 @@
  * Page product slug
  */
 
-export default function ProductPage() {
+import { initialData } from "@/seed/seed";
+import { titleFont } from "@/config/fonts";
+import { notFound } from "next/navigation";
+
+interface Props {
+    params: {
+        slug: string
+    }
+}
+
+const seedProduct = initialData.products;
+
+export default function ProductPage({ params }: Props ) {
+    const { slug } = params;
+
+    const product = seedProduct.find( product => {
+        return product.slug === slug 
+    });
+
+    console.log("🚀 ~ ProductPage ~ product:", product)
+    
+    if (!product) {
+        notFound();
+    }
+
     return (
-        <div>
-            <h1>Hello ProductPage slug 🚀 </h1>
+        <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+            {/* SlideShows */}
+
+            <div className="col-span-1 md:col-span-2">
+                <h1>hola 🚀 </h1>
+            </div>
+
+            {/* Details */}
+            <div className="col-span-1 px-5">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+                    { product.title }
+                </h1>
+                <p className="text-lg mb-5">
+                    ${ product.price.toFixed(2) }
+                </p>
+
+                {/* Selector de tallas */}
+
+
+                {/* Selector de cantidades */}
+
+                {/* Button */}
+                <button className="btn-primary my-5">
+                    Agregar al carrito
+                </button>
+
+                {/* Description */}
+                <h3 className="font-bold text-sm">Descripcion</h3>
+                <p className="font-light mt-2">
+                    {product.description}
+                </p>
+            </div>
         </div>
     );
 }
