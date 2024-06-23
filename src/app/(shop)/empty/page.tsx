1b0d452fb2ec93e45c1ad1079ca6d676043c5bad
fredo09 <fred_15@outlook.com>
2024-06-23 00:16:10 -0600

    add empty page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -2,11 +2,30 @@
  * Page empty cart
  */
 
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 
 export default function EmptyPage() {
   return (
-    <div>
-      <h1>Hello Empty cart 🚀 </h1>
+    <div className="flex justify-center items-center h-[800px]">
+
+      <IoCartOutline size={80} className="mx-5" />
+
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">
+          Tu carrito está vacío
+        </h1>
+
+        <Link
+          href='/'
+          className="text-blue-500 mt-2 text-4xl">
+          Volver al home!
+        </Link>
+
+      </div>
+
+
     </div>
   );
 }
