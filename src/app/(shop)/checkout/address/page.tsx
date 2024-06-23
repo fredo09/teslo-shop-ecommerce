331bbc2d6a4ea51528c36ba1594808d2a865b3cd
fredo09 <fred_15@outlook.com>
2024-06-23 00:00:11 -0600

    modify address page

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -2,10 +2,100 @@
  * Page address
  */
 
+import { Title } from '@/components';
+import Link from 'next/link';
+
 export default function AddressPage() {
     return (
-        <div>
-            <h1>Hello Address 🚀 </h1>
+        <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+
+            <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
+
+                <Title title="Dirección" subTitle="Dirección de entrega" />
+
+                <div className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
+
+
+                    <div className="flex flex-col mb-2">
+                        <span>Nombres</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>Apellidos</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>Dirección</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>Dirección 2 (opcional)</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+
+                    <div className="flex flex-col mb-2">
+                        <span>Código postal</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>Ciudad</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>País</span>
+                        <select
+                            className="p-2 border rounded-md bg-gray-200"
+                        >
+                            <option value="">[ Seleccione ]</option>
+                            <option value="CRI">Costa Rica</option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col mb-2">
+                        <span>Teléfono</span>
+                        <input
+                            type="text"
+                            className="p-2 border rounded-md bg-gray-200"
+                        />
+                    </div>
+
+
+                    <div className="flex flex-col mb-2 sm:mt-10">
+                        <Link
+                            href='/checkout'
+                            className="btn-primary flex w-full sm:w-1/2 justify-center ">
+                            Siguiente
+                        </Link>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
     );
 }
