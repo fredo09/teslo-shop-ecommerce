51c0022960dc1e9dc8e2c4b2953a049d279bc213
fredo09 <fred_15@outlook.com>
2024-06-23 00:38:11 -0600

    add login page and account page

--- src/app/auth/newAccount/page.tsx
+++ src/app/auth/newAccount/page.tsx
@@ -1,18 +1,19 @@
-/**
- * Page Login
- */
+import { titleFont } from "@/config/fonts";
+import Link from "next/link";
 
-
-import Link from 'next/link';
-import { titleFont } from '@/config/fonts';
-
-export default function LoginPage() {
+export default function NewAccountPage() {
     return (
         <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
-            <h1 className={`${titleFont.className} text-4xl mb-5`}>Ingresar</h1>
+            <h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
 
             <div className="flex flex-col">
+
+                <label htmlFor="email">Nombre completo</label>
+                <input
+                    className="px-5 py-2 border bg-gray-200 rounded mb-5"
+                    type="text" />
+
 
                 <label htmlFor="email">Correo electrónico</label>
                 <input
@@ -24,16 +25,11 @@ export default function LoginPage() {
                 <input
                     className="px-5 py-2 border bg-gray-200 rounded mb-5"
                     type="email" />
-                
-                {/* <label htmlFor="email">Repetir Contraseña</label>
-                <input
-                    className="px-5 py-2 border bg-gray-200 rounded mb-5"
-                    type="email" /> */}
 
                 <button
 
                     className="btn-primary">
-                    Ingresar
+                    Crear cuenta
                 </button>
 
 
@@ -45,9 +41,9 @@ export default function LoginPage() {
                 </div>
 
                 <Link
-                    href="/auth/new-account"
+                    href="/auth/login"
                     className="btn-secondary text-center">
-                    Crear una nueva cuenta
+                    Ingresar
                 </Link>
 
             </div>
