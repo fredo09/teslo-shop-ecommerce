f655ca32bd5e13c992b923f804bf91a1ad770fc5
fredo09 <fred_15@outlook.com>
2024-06-30 21:40:47 -0600

    add middleware persist of zustand

--- src/store/cartStore/cart-store.ts
+++ src/store/cartStore/cart-store.ts
@@ -4,6 +4,7 @@
 
 import { create } from "zustand"
 import type { CartStore } from "@/interfaces";
+import { persist } from "zustand/middleware";
 
 //* tipado del estado del zustand
 interface State {
@@ -18,43 +19,49 @@ interface State {
 
 
 export const useCartStore = create<State>()(
-    //* set y get son funciones del mismo zustand
-    (set, get) => ({
-        cart: [],
+    persist( //! -> Nos sirve para agregar el store al localStorage
+        //* set y get son funciones del mismo zustand
+        (set, get) => ({
+            cart: [],
 
-        //* Actions
-        addToProductCart: ( product: CartStore ) => {
-            const { cart } = get();
+            //* Actions
+            addToProductCart: (product: CartStore) => {
+                const { cart } = get();
+                console.log("🚀 ~ cart:", cart);
 
-            //! 1. Si cumple con el id y con la talla seleccionada es que el producto existe en el carrito 
-            const productIncart = cart.some( item => item.id === product.id && item.size === product.size );
+                //! 1. Si cumple con el id y con la talla seleccionada es que el producto existe en el carrito 
+                const productIncart = cart.some(item => item.id === product.id && item.size === product.size);
 
-            //! Si no hay producto agregamos al carrito
-            if ( !productIncart ) {
-                set({
-                    cart: [...cart, product]
-                })
-            }
-
-            //! 2. Se que el producto existe por talla asi que... tengo que insertar
-            const updateCountProductInCart = cart.map((item) => {
-                //* Hacemos la operaccion de agregar la cantidad al carrito del producto "si cumple la condicion"
-                if ( item.id === product.id && item.size === product.size ) {
-                    return {
-                        ...item,
-                        quantity: item.quantity + product.quantity
-                    }
+                //! Si no hay producto agregamos al carrito
+                if (!productIncart) {
+                    set({
+                        cart: [...cart, product]
+                    })
+                    return;
                 }
 
-                //* Si no cumple con la condicion regresamos el item tal cual 
-                return item;
-            });
+                //! 2. Se que el producto existe por talla asi que... tengo que insertar
+                const updateCountProductInCart = cart.map((item) => {
+                    //* Hacemos la operaccion de agregar la cantidad al carrito del producto "si cumple la condicion"
+                    if (item.id === product.id && item.size === product.size) {
+                        return {
+                            ...item,
+                            quantity: item.quantity + product.quantity
+                        }
+                    }
 
-            //* Nuevo valor del arreglo del carrito
-            set({
-                cart: updateCountProductInCart
-            });
+                    //* Si no cumple con la condicion regresamos el item tal cual 
+                    return item;
+                });
+
+                //* Nuevo valor del arreglo del carrito
+                set({
+                    cart: updateCountProductInCart
+                });
+            }
+        })
+        ,{
+            name: 'shopping-cart'
         }
-
-    })
+    )
 )
