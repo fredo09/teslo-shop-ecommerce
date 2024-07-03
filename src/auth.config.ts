9009b767b59b6ec8b789b257b47ff52b748ed9fa
fredo09 <fred_15@outlook.com>
2024-07-02 21:09:54 -0600

    add provider credencial for email and password data in login config NextAuth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,12 +2,40 @@
  * Config auth.hs with nextAtuh
  */
 
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import { z } from 'zod';
+import Credentials from 'next-auth/providers/credentials';
 
 export const authConfig: NextAuthConfig = {
     pages: {
         signIn: '/auth/login',
         newUser: '/auth/newAccount'
     },
-    providers: []
+    providers: [
+        //! login con credenciales de email y contraseña
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(8) })
+                    .safeParse(credentials);
+                
+                console.log("🚀 ~ authorize ~ parsedCredentials:", parsedCredentials)
+
+                if (!parsedCredentials) return null;
+
+                const { data, error, success } = parsedCredentials;
+
+                //* BUSCAR USUARIO
+
+                //* COMPARAR CONTRASEÑAS
+
+                //* Regresar el usuario informacion necesaria
+
+                return null;
+            },
+        }),
+    ]
 };
+
+//* exportamos la configuracion del auth
+export const { signIn, signOut, auth } = NextAuth(authConfig);
