e9ba43e4538965e948f8006fbd61a78c2a782947
fredo09 <fred_15@outlook.com>
2024-07-15 22:13:57 -0600

    add new comment

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -4,6 +4,10 @@
 
 import NextAauth, { DefaultSession } from 'next-auth';
 
+
+/**
+ * Esto se usa para declarar una nueva forma de escribir una interface session para la authentificacion
+ */
 declare module 'next-auth' {
     interface Session {
         user : {
