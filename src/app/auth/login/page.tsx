352858c5bc065d3e2ccba20a6f1b83ac6529e890
fredo09 <fred_15@outlook.com>
2024-07-15 23:36:53 -0600

    update page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,7 +7,7 @@ import { LoginFrom } from './ui/LoginForm/index';
 
 export default function LoginPage() {
     return (
-        <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+        <div className="flex flex-col min-h-screen pt-24 sm:pt-40">
 
             <h1 className={`${titleFont.className} text-4xl mb-5`}>Ingresar</h1>
 
