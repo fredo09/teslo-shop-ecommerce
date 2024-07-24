3bc2ab5940606b477aabcc188f80b99449ba2146
fredo09 <fred_15@outlook.com>
2024-07-23 22:04:35 -0600

    add server action country in page address

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -3,17 +3,25 @@
  */
 
 import { Title } from '@/components';
-import Link from 'next/link';
 import { AddressForm } from './ui/AddressForm';
+import { getCountriesAction } from '@/actions';
 
-export default function AddressPage() {
+//Interfaces
+import { Country } from '@/interfaces';
+
+export default async function AddressPage() { 
+
+    const contries: Country[] = await getCountriesAction();
+
     return (
         <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
             <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
                 <Title title="Dirección" subTitle="Dirección de entrega" />
 
                 {/* TODO: AQUI SE MOSTRAR LA LISTA DE DIRECCIONES O INCLUSO MOSTRAR LA DIRECCION DE DEFAULT */}
-                <AddressForm/>
+                <AddressForm 
+                    countries={contries}
+                />
             </div>
         </div>
     );
