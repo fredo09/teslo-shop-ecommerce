02456678687083d2df09e3bb0d6aab07e16f8023
fredo09 <fred_15@outlook.com>
2024-07-25 22:08:16 -0600

    add session user and server actions for getUserAddress

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -3,8 +3,9 @@
  */
 
 import { Title } from '@/components';
+import { auth } from '@/auth.config';
 import { AddressForm } from './ui/AddressForm';
-import { getCountriesAction } from '@/actions';
+import { getAddressUserAcction, getCountriesAction } from '@/actions';
 
 //Interfaces
 import { Country } from '@/interfaces';
@@ -12,6 +13,18 @@ import { Country } from '@/interfaces';
 export default async function AddressPage() { 
 
     const contries: Country[] = await getCountriesAction();
+
+    //* -> Session del usuario con auth en paginas cargadas desde el servidor
+    const sessionData = await auth();
+
+    if (!sessionData?.user) {
+        return(
+            <h3 className='text-5xl'>500 - No hay session</h3>
+        )
+    }
+    
+    //* Server actions
+    const getAddress = await getAddressUserAcction(sessionData.user.id) ?? undefined;
 
     return (
         <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
@@ -21,6 +34,7 @@ export default async function AddressPage() {
                 {/* TODO: AQUI SE MOSTRAR LA LISTA DE DIRECCIONES O INCLUSO MOSTRAR LA DIRECCION DE DEFAULT */}
                 <AddressForm 
                     countries={contries}
+                    userAddress={getAddress}
                 />
             </div>
         </div>
