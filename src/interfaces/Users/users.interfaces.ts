3a72e6ef665034e82013c8f6dc8c833872122541
fredo09 <fred_15@outlook.com>
2024-08-05 00:00:58 -0600

    add new interface users

---
+++ src/interfaces/Users/users.interfaces.ts
@@ -0,0 +1,13 @@
+/**
+ * interfaces users 
+ */
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    emailVerified?: Date | null;
+    password: string;
+    role: string;
+    image?: string | null;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,3 +6,4 @@ export * from './Address/address.interface';
 export * from './Country/country.interface';
 export * from './Paypal/Paypal.interface';
 export * from './Products/products.interface';
+export * from './Users/users.interfaces';
