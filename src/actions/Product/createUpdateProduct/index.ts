7ea10784504ace23ab1c89c29320662b54772a91
fredo09 <fred_15@outlook.com>
2024-08-08 22:59:21 -0600

    add new server action

---
+++ src/actions/Product/createUpdateProduct/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Server action Product create and update 
+ */
+'use server';
+
+export const createUpdateProductAction = async(formDataProduct: FormData) => {
+    console.log("🚀 ~ createUpdateProductAction ~ formDataProduct:", formDataProduct)
+};
--- src/actions/Product/index.ts
+++ src/actions/Product/index.ts
@@ -2,6 +2,7 @@
  * exports product actions
  */
 
+export * from './createUpdateProduct';
 export * from './productBySlug';
 export * from './productPagination';
 export * from './productStockBySlug';
